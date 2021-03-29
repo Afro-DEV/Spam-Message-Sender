9133e213c1d3390caf69775856b2ec6d3b8eb483
Ibitayo <ibitayosalawu@gmail.com>
2021-03-29 19:50:26 +0100

    Spam message

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace SpamBot
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.START = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// START
+			// 
+			this.START.BackColor = System.Drawing.Color.Black;
+			this.START.ForeColor = System.Drawing.Color.Lime;
+			this.START.Location = new System.Drawing.Point(262, 32);
+			this.START.Name = "START";
+			this.START.Size = new System.Drawing.Size(115, 23);
+			this.START.TabIndex = 0;
+			this.START.Text = "START";
+			this.START.UseVisualStyleBackColor = false;
+			this.START.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.SystemColors.Desktop;
+			this.button2.ForeColor = System.Drawing.Color.Red;
+			this.button2.Location = new System.Drawing.Point(262, 74);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(115, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "STOP";
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(12, 49);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(217, 20);
+			this.textBox1.TabIndex = 2;
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.label1.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.Aqua;
+			this.label1.Location = new System.Drawing.Point(16, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(213, 26);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "SpamBot made by Afro";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.ForeColor = System.Drawing.Color.White;
+			this.label2.Location = new System.Drawing.Point(9, 72);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(108, 13);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Enter Spam Message";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.ClientSize = new System.Drawing.Size(389, 136);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.START);
+			this.Name = "Form1";
+			this.Text = "Afro Spam Bot";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button START;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+	}
+}
+
